776649f1456ce2b1fa64c025ed7df8f8da1158a9
Nimmy Issac <nimmyissac2014@gmail.com>
2016-11-21 05:46:13 -0800

    React Components Completed

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// TODO
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map( item =>
+       <GroceryListItem groceryItem = {item} />
+    )}
+  </ul>
+)
+
+    
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    	done: false,
+    	hover: false
+    }
+  }
+  onclick () {
+  	this.setState({done: !this.state.done})
+  }
+  mouseOver () {
+    this.setState({hover: true});
+  }
+  mouseOut () {
+    this.setState({hover: false});
+  }
+  render() {
+  	var style = {'text-decoration': this.state.done ? 'line-through' : 'none', 'font-weight' : this.state.hover ? 'bold' : 'normal'}
+  	return  (<li style = {style} onClick ={this.onclick.bind(this)} onMouseOver = {this.mouseOver.bind(this)} onMouseOut = {this.mouseOut.bind(this)}> {this.props.groceryItem} </li>);
+  }
+ 
+
+}
+
+ReactDOM.render(<GroceryList items={['Rice', 'Pasta']}/> , document.getElementById('app'));
